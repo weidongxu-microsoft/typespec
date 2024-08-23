353ef149a421ca8c6ccee77145812b98d85705e1
M1lney <tomj.milne@gmail.com>
2024-08-23 19:54:48 +1200

    Pass some data as an example. #4 #5

--- packages/java-spring/src/spring/components/rest-controller.tsx
+++ packages/java-spring/src/spring/components/rest-controller.tsx
@@ -9,10 +9,14 @@ export interface RestControllerProps {
 }
 
 export function RestController(props: RestControllerProps) {
+  const params:Record<string, string> = {"String" : "sortBy"};
   return <>
     <jv.Annotation type={"RestController"}></jv.Annotation>
     <jv.Class name={props.nameSpace ? `${props.nameSpace.name}Controller` : `${props.name}Controller`}>
-      <RouteHandler call={"GET"} route={"/people"} name={"all"} return={"List<Person>"}></RouteHandler>
+
+      <RouteHandler call={"GET"} route={"/people"} name={"listPeople"}
+                    return={"List<Person>"} parameters={params} children={""}>
+      </RouteHandler>
     </jv.Class>
   </>
 }
