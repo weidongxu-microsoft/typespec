c6874bf648144259f05d132a9a0e2de2f7bb7c8a
Jamin <jaminstratford@gmail.com>
2024-09-03 19:37:29 +1200

    start using http library to query operations

--- packages/java-spring/src/emitter.tsx
+++ packages/java-spring/src/emitter.tsx
@@ -1,14 +1,40 @@
 import * as ay from "@alloy-js/core";
 import * as jv from "@alloy-js/java";
 import { MavenProjectConfig } from "@alloy-js/java";
-import { EmitContext, Model, Namespace, Operation, StringValue } from "@typespec/compiler";
+import { EmitContext, Model, Operation } from "@typespec/compiler";
 import { isArray, TypeCollector } from "@typespec/emitter-framework";
 import { ModelSourceFile } from "@typespec/emitter-framework/java";
+import {
+  getAllHttpServices,
+  getRoutePath,
+  HttpOperation,
+  OperationContainer,
+} from "@typespec/http";
 import fs from "node:fs";
 import { mkdir, rm, writeFile } from "node:fs/promises";
 import { dirname, join } from "node:path";
 import { SpringProject } from "./spring/components/index.js";
 import { springFramework } from "./spring/libraries/index.js";
+
+const projectConfig: MavenProjectConfig = {
+  groupId: "io.typespec",
+  artifactId: "generated",
+  version: "1.0.0",
+  javaVersion: 8,
+  build: {
+    plugins: [
+      {
+        groupId: "org.springframework.boot",
+        artifactId: "spring-boot-maven-plugin",
+      },
+    ],
+  },
+};
+
+interface OperationsGroup {
+  container?: OperationContainer;
+  operations: HttpOperation[];
+}
 
 /**
  * Just leaving my general thought notes here:
@@ -21,39 +47,40 @@ import { springFramework } from "./spring/libraries/index.js";
  * - Repositories (Way to retrieve data from where it's stored, database etc)
  */
 export async function $onEmit(context: EmitContext) {
-  // Get all types in program, categorize them into models, operations, etc based on tags
-  // Generate in respective sections of program.
-
+  // Query types needed in program, models, interfaces etc
   const types = queryTypes(context);
 
-  // types.dataTypes.forEach((dataType, index) => {
-  //   console.log(`\n======= Entry ${index} =======`);
-  //   console.log(`Data Type Name: ${dataType.name}`);
-  //   console.log("Details:", dataType);
-  //   console.log("Decorators:", dataType.decorators);
-  //   console.log("===============================\n");
-  // });
+  // Get all http services, then collect all routes from services
+  const services = getAllHttpServices(context.program);
 
-  const projectConfig: MavenProjectConfig = {
-    groupId: "net.microsoft",
-    artifactId: "generated",
-    version: "1.0.0",
-    javaVersion: 8,
-    build: {
-      plugins: [
-        {
-          groupId: "org.springframework.boot",
-          artifactId: "spring-boot-maven-plugin",
-        },
-      ],
+  // TODO: For now just take first service
+  const service = services[0][0];
+
+  // Collect HttpOperations by container
+  const httpOperations = service.operations.reduce(
+    (acc, op) => {
+      const namespaceKey = op.container?.name ?? "";
+      if (!acc[namespaceKey]) {
+        acc[namespaceKey] = {
+          container: op.container,
+          operations: [],
+        };
+      }
+
+      let operations = acc[namespaceKey].operations;
+      operations.push(op);
+      acc[namespaceKey] = { ...acc[namespaceKey], operations };
+      return acc;
     },
-  };
+    {} as Record<string, OperationsGroup>
+  );
 
-  // TODO: Indent is really weird in generated output
+  // TODO: Handle array of services, are we generating all in same project, and just compiling
+  // TODO: all operations into one? Or are we generating different java projects for each?
   const result = ay.render(
     <ay.Output externals={[springFramework]}>
       <SpringProject name="TestProject" mavenProjectConfig={projectConfig}>
-        <jv.PackageDirectory package="me.test.code">
+        <jv.PackageDirectory package="io.typespec.generated">
           <jv.SourceFile path="MainApplication.java">
             <jv.Annotation type={springFramework.SpringBootApplication} />
             <jv.Class public name="MainApplication">
@@ -68,7 +95,7 @@ export async function $onEmit(context: EmitContext) {
             ))}
           </jv.PackageDirectory>
           <jv.PackageDirectory package="controllers">
-            {emitOperations(types.ops)}
+            {emitOperations(context, httpOperations)}
           </jv.PackageDirectory>
         </jv.PackageDirectory>
       </SpringProject>
@@ -78,58 +105,35 @@ export async function $onEmit(context: EmitContext) {
   await writeOutput(result, "./tsp-output", false);
 }
 
-interface NamespaceOperations {
-  namespace?: Namespace;
-  operations: Operation[];
-}
-
 /**
- * Collect operations by namespace. Create RouteHandler for namespace. Declare those
- * operations in that route handler as the service endpoints.
+ * Emit route handlers for http operations. Takes an OperationsGroup
  *
- * @param ops
+ * @param ops List of http operations
  */
-function emitOperations(ops: Operation[]) {
-  const operationsByNamespace = ops.reduce(
-    (acc, op) => {
-      const namespaceKey = op.namespace?.name ?? "";
-      if (!acc[namespaceKey]) {
-        acc[namespaceKey] = {
-          namespace: op.namespace,
-          operations: [],
-        };
-      }
-
-      let operations = acc[namespaceKey].operations;
-      operations.push(op);
-      acc[namespaceKey] = { ...acc[namespaceKey], operations };
-      return acc;
-    },
-    {} as Record<string, NamespaceOperations>
-  );
-
+function emitOperations(context: EmitContext, ops: Record<string, OperationsGroup>) {
   return (
     <>
-      {Object.values(operationsByNamespace).map((nsOps) => {
-        const routePath = (
-          nsOps?.namespace?.decorators?.find((d) => d?.definition?.name === "@route")?.args?.[0]
-            ?.value as StringValue
-        )?.value;
+      {Object.values(ops).map((nsOps) => {
+        if (nsOps.container === undefined) return null;
 
+        // Get route decorator on container
+        const routePath = getRoutePath(context.program, nsOps.container)?.path;
+
+        // TODO: Oncall RouteHandler that takes HttpOperation type. Can query everything off that to construct the route handler
         return (
-          <jv.SourceFile path={nsOps.namespace?.name + "Controller.java"}>
+          <jv.SourceFile path={nsOps.container?.name + "Controller.java"}>
             <jv.Annotation type={springFramework.RestController} />
             <jv.Annotation
               type={springFramework.RequestMapping}
               value={{ path: <jv.Value value={routePath} /> }}
             />
-            <jv.Class public name={nsOps.namespace?.name + "Controller"}>
+            <jv.Class public name={nsOps.container?.name + "Controller"}>
               {nsOps.operations.map((op) => {
                 return (
                   <>
                     <jv.Annotation type={springFramework.GetMapping} />
-                    <jv.Method public name={op.name}>
-                      throw new UnsupportedOperationException("Not implemented");
+                    <jv.Method public return="String" name={op.operation?.name}>
+                      return "Hello, World!";
                     </jv.Method>
                   </>
                 );
