bc46441d50c8fe1f0a2a924794bed5d448c75c41
M1lney <tomj.milne@gmail.com>
2024-09-18 16:36:16 +1200

    Remove unused code for collecting annotations. #14

--- packages/java-spring/src/spring/components/annotations.tsx
+++ packages/java-spring/src/spring/components/annotations.tsx
@@ -1,8 +1,7 @@
 import * as jv from "@alloy-js/java"
 import { Child, Refkey } from "@alloy-js/core";
 import { springFramework } from "../libraries/index.js";
-import { HttpOperation, HttpOperationParameter, HttpProperty } from "@typespec/http";
-import { TypeExpression } from "@typespec/emitter-framework/java";
+
 
 export interface AnnotationsProps {
   annotationKind: string;
@@ -31,51 +30,3 @@ export const SpringAnnotations = new Map<string, Refkey>([
   ["query", springFramework.RequestParam],
 ])
 
-export function collectAnnotations(op: HttpOperation) {
-  
-  const route = op.verb;
-  const path = <jv.Value value={op.uriTemplate}/>;
-  
-  const routeAnnotation = <SpringAnnotation annotationKind={route} annotationParameters={path}></SpringAnnotation>
-
-  /**filters out parameter properties taken from a model type. If the parameter type is a model
-   * then all the properties of that model will be present in parameters.properties.
-  */
-  const parameters = op.parameters.parameters;
-
-  const parameterAnnotations = parameters.map(httpParam => ({
-    parameter: httpParam,
-    annotation: getParamAnnotation(httpParam),
-  }));
-
-  const body = op.parameters.body;
-  const bodyAnnotationKind = body?.bodyKind == "single" ? "body" : "multipartBody";
-  const bodyParamAnnotation = <SpringAnnotation annotationKind={bodyAnnotationKind}/>
-
-  return {
-    routeAnnotation,
-    parameterAnnotations,
-    body,
-    bodyParamAnnotation
-  };
-
-}
-
-function getParamAnnotation(parameter: HttpOperationParameter) {
-
-  let annotationKind : string;
-  let annotationParameters : string | undefined;
-
-  switch (parameter.type) {
-    case "header":
-    case "query":
-      annotationKind = parameter.type;
-      annotationParameters = parameter.name;
-      break;
-    case "path":
-      annotationKind = parameter.type;
-      break;
-  }
-
-  return <SpringAnnotation annotationKind={annotationKind} annotationParameters={annotationParameters}/>
-}
