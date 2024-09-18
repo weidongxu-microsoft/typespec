d8a8027dbd34b7b7fb306ce886809cb62eb7f8b1
M1lney <tomj.milne@gmail.com>
2024-09-18 16:40:23 +1200

    Remove unused code and update to use endpoint parameters. #14

--- packages/java-spring/src/spring/components/spring-service-endpoint.tsx
+++ packages/java-spring/src/spring/components/spring-service-endpoint.tsx
@@ -4,7 +4,7 @@ import {
 import * as jv from "@alloy-js/java"
 import { Children } from "@alloy-js/core";
 import { TypeExpression } from "@typespec/emitter-framework/java";
-import { collectAnnotations, SpringAnnotations } from "./annotations.js";
+import { SpringAnnotation, SpringAnnotations } from "./annotations.js";
 import { $ } from "@typespec/compiler/typekit";
 import { SpringEndpointParameters } from "./spring-service-endpoint-parameters.js";
 export interface SpringServiceEndpointProps{
@@ -14,32 +14,15 @@ export interface SpringServiceEndpointProps{
 
 export function SpringServiceEndpoint({ op, children }: SpringServiceEndpointProps) {
 
-  const annotations = collectAnnotations(op);
+  const route = op.verb;
+  const path = <jv.Value value={op.uriTemplate}/>;
 
-  const parameters = $.httpRequest.getParameters(op, ["path", "query", "header", "contentType"]);
-  if (parameters && parameters.properties) {
-    //console.log(parameters.properties.values());
-  }
-  const paramRecord: Record<string, string> = {};
-
-  for (const param of annotations.parameterAnnotations) {
-
-    const property = param.parameter.param;
-    const paramName = param.parameter.name;
-    const paramType = <TypeExpression type={property}></TypeExpression>
-
-    paramRecord[paramName] = <>{param.annotation} {paramType}</>;
-  }
-
-  if (annotations.body && annotations.body.property) {
-    const bodyType = <TypeExpression type={annotations.body.property}></TypeExpression>
-    paramRecord[annotations.body.property?.name] = <>{annotations.bodyParamAnnotation} {bodyType}</>
-  }
+  const routeAnnotation = <SpringAnnotation annotationKind={route} annotationParameters={path}></SpringAnnotation>
 
   const springParams = SpringEndpointParameters(op);
   return(
     <>
-      <>{annotations.routeAnnotation}</>
+      <>{routeAnnotation}</>
       <jv.Method name={op.operation.name} parameters={springParams} children={children}>
       </jv.Method>
       <>{`\n`}</>
