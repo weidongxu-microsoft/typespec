1c084595ed12ec2ee88f0d610fb6f461b439b7a6
M1lney <tomj.milne@gmail.com>
2024-09-18 16:44:04 +1200

    Update parameter options to use alloy value component. #14

--- packages/java-spring/src/spring/components/spring-service-endpoint-parameters.tsx
+++ packages/java-spring/src/spring/components/spring-service-endpoint-parameters.tsx
@@ -21,27 +21,30 @@ export function SpringEndpointParameters(op: HttpOperation) {
 
   if (bodyParams && bodyParams.property) {
     const name = bodyParams.property?.name ?? "" ;
-    const annotation = <SpringAnnotation annotationKind={"body"}/>
-    paramRecord[name] = <>{annotation} <TypeExpression type={bodyParams.property}/></>
+    const annotation = <SpringAnnotation annotationKind={"body"}/>;
+    paramRecord[name] = <>{annotation} <TypeExpression type={bodyParams.property}/></>;
   }
   if (headerParams) {
     for (const headerParam of headerParams.properties.values()) {
       const options = $.modelProperty.getHttpParamOptions(headerParam) as HeaderFieldOptions;
-      const annotation = <SpringAnnotation annotationKind={"header"} annotationParameters={options.name}/>
+      const optionValue = <jv.Value value={options.name}></jv.Value>;
+      const annotation = <SpringAnnotation annotationKind={"header"} annotationParameters={optionValue}/>;
       paramRecord[headerParam.name] = <>{annotation} <TypeExpression type={headerParam}></TypeExpression></>;
     }
   }
   if (pathParams) {
     for (const pathParam of pathParams.properties.values()) {
       const options = $.modelProperty.getHttpParamOptions(pathParam) as PathParameterOptions;
-      const annotation = <SpringAnnotation annotationKind={"path"} annotationParameters={options.name}/>
+      const optionValue = <jv.Value value={options.name}></jv.Value>;
+      const annotation = <SpringAnnotation annotationKind={"path"} annotationParameters={optionValue}/>;
       paramRecord[pathParam.name] = <>{annotation} <TypeExpression type={pathParam}></TypeExpression></>;
     }
   }
   if (queryParams) {
     for (const queryParam of queryParams.properties.values()) {
       const options = $.modelProperty.getHttpParamOptions(queryParam) as QueryParameterOptions;
-      const annotation = <SpringAnnotation annotationKind={"query"} annotationParameters={options.name}/>
+      const optionValue = <jv.Value value={options.name}></jv.Value>;
+      const annotation = <SpringAnnotation annotationKind={"query"} annotationParameters={optionValue}/>;
       paramRecord[queryParam.name] = <>{annotation} <TypeExpression type={queryParam}></TypeExpression></>;
     }
   }
