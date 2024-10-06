7679d6436db9a5015ba31ee38c7582fb64a1e003
M1lney <tomj.milne@gmail.com>
2024-10-06 15:16:12 +1300

    Implement support for custom values for enum members #26

--- packages/emitter-framework/src/java/components/enum.tsx
+++ packages/emitter-framework/src/java/components/enum.tsx
@@ -1,6 +1,7 @@
 import { Enum } from "@typespec/compiler";
 import * as jv from  "@alloy-js/java"
 import { mapJoin } from "@alloy-js/core";
+import { TypeExpression } from "../../typescript/index.js";
 
 
 export interface EnumProps {
@@ -10,11 +11,47 @@ export interface EnumProps {
 export function EnumDeclaration({ type }: EnumProps) {
 
   const members = Array.from(type.members.values());
+
+  let valueType: string | undefined;
+  let isConsistent = true;
+
+  members.forEach((member, index) => {
+    if (member.value !== undefined) {
+      const currentType = typeof member.value === "string" ? "String" : "int";
+
+      if (index === 0) {
+        valueType = currentType;
+      } else if (valueType !== currentType) {
+        isConsistent = false;
+      }
+    }
+  });
+
+  if (!isConsistent) {
+    throw new Error("All enum values must have the same type");
+  }
+
+  let constructor = null;
+  let variable = null;
+  if (valueType) {
+    variable = <jv.Variable type={valueType} name="customValue" private={true}/>;
+    constructor = <jv.Constructor name={type.name} parameters={{"customValue": valueType}}
+                                  children={"this.customValue = customValue;"} private={true}/>;
+  }
+
   const joinedMembers = mapJoin(members, (member) => {
-    return <jv.EnumMember name={member.name}></jv.EnumMember>
+    const customValue = member.value ? <jv.Value value={member.value}/> : null;
+    return <jv.EnumMember name={member.name} arguments={customValue}/>
   }, { joiner: ",\n" });
 
-  return <jv.Enum name={type.name}>
+
+
+  return valueType ? <jv.Enum name={type.name}>
+    {joinedMembers};
+
+    {variable}
+    {constructor}
+  </jv.Enum> : <jv.Enum name={type.name}>
     {joinedMembers}
   </jv.Enum>
 }
