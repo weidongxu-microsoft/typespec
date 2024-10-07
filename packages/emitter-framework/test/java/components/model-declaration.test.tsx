6824c453986f056a3835a83c461de654b3e8b932
Jamin <jaminstratford@gmail.com>
2024-10-07 16:01:17 +1300

    tests for extending models

--- packages/emitter-framework/test/java/components/model-declaration.test.tsx
+++ packages/emitter-framework/test/java/components/model-declaration.test.tsx
@@ -1,8 +1,9 @@
 import { d } from "@alloy-js/core/testing";
+import { PackageDirectory, SourceFile } from "@alloy-js/java/stc";
 import { Model, Namespace } from "@typespec/compiler";
 import { describe, expect, it } from "vitest";
 import { ModelDeclaration } from "../../../src/java/index.js";
-import { getEmitOutput } from "../utils.js";
+import { getEmitOutput, getMultiEmitOutput } from "../utils.js";
 
 describe("TypeSpec Model Declaration", () => {
   it("Takes a model type parameter", async () => {
@@ -112,6 +113,174 @@ describe("TypeSpec Model Declaration", () => {
         
         public void setItem(T item) {
           this.item = item;
+        }
+        
+      }
+    `);
+  });
+
+  it("Extends a model", async () => {
+    const code = `
+        namespace DemoService;
+        model BaseModel {
+          value: string;
+        }
+        
+        model Widget extends BaseModel {
+          id: string;
+        }
+    `;
+
+    const output = await getMultiEmitOutput(code, (program) => {
+      const [namespace] = program.resolveTypeReference("DemoService");
+      const Widget = Array.from((namespace as Namespace).models.values())[1];
+      return [
+        SourceFile({ path: "Test.java" }).children(() => {
+          return <ModelDeclaration type={Widget} />;
+        }),
+        PackageDirectory({ package: "base" }).children(
+          SourceFile({ path: "Test2.java" }).children(() => {
+            return <ModelDeclaration type={Widget.baseModel!} />;
+          }),
+        ),
+      ];
+    });
+
+    expect(output).toBe(d`
+      package me.test.code;
+      
+      import me.test.code.base.BaseModel;
+      
+      public class Widget extends BaseModel {
+        
+        private String id;
+        
+        public Widget() {
+          
+        }
+        
+        public Widget(String id) {
+          this.id = id;
+        }
+        
+        public String getId() {
+          return this.id;
+        }
+        
+        public void setId(String id) {
+          this.id = id;
+        }
+        
+      }
+    `);
+  });
+
+  it("Extends a generic model", async () => {
+    const code = `
+        namespace DemoService;
+        model BaseModel<T> {
+          value: T;
+        }
+        
+        model Widget extends BaseModel<string> {
+          id: string;
+        }
+    `;
+
+    const output = await getMultiEmitOutput(code, (program) => {
+      const [namespace] = program.resolveTypeReference("DemoService");
+      const Widget = Array.from((namespace as Namespace).models.values())[1];
+      return [
+        SourceFile({ path: "Test.java" }).children(() => {
+          return <ModelDeclaration type={Widget} />;
+        }),
+        PackageDirectory({ package: "base" }).children(
+          SourceFile({ path: "Test2.java" }).children(() => {
+            return <ModelDeclaration type={Widget.baseModel!} />;
+          }),
+        ),
+      ];
+    });
+
+    expect(output).toBe(d`
+      package me.test.code;
+      
+      import me.test.code.base.BaseModel;
+      
+      public class Widget extends BaseModel<String> {
+        
+        private String id;
+        
+        public Widget() {
+          
+        }
+        
+        public Widget(String id) {
+          this.id = id;
+        }
+        
+        public String getId() {
+          return this.id;
+        }
+        
+        public void setId(String id) {
+          this.id = id;
+        }
+        
+      }
+    `);
+  });
+
+  it("Extends a generic model and passes generic type", async () => {
+    const code = `
+        namespace DemoService;
+        model BaseModel<T> {
+          value: T;
+        }
+        
+        model Widget<T> extends BaseModel<T> {
+          id: T;
+        }
+    `;
+
+    const output = await getMultiEmitOutput(code, (program) => {
+      const [namespace] = program.resolveTypeReference("DemoService");
+      const Widget = Array.from((namespace as Namespace).models.values())[1];
+      return [
+        SourceFile({ path: "Test.java" }).children(() => {
+          return <ModelDeclaration type={Widget} />;
+        }),
+        PackageDirectory({ package: "base" }).children(
+          SourceFile({ path: "Test2.java" }).children(() => {
+            return <ModelDeclaration type={Widget.baseModel!} />;
+          }),
+        ),
+      ];
+    });
+
+    expect(output).toBe(d`
+      package me.test.code;
+      
+      import me.test.code.base.BaseModel;
+      
+      public class Widget<T> extends BaseModel<T> {
+        
+        private T id;
+        
+        public Widget() {
+          
+        }
+        
+        public Widget(T id) {
+          this.id = id;
+        }
+        
+        public T getId() {
+          return this.id;
+        }
+        
+        public void setId(T id) {
+          this.id = id;
         }
         
       }
--- packages/emitter-framework/test/java/utils.ts
+++ packages/emitter-framework/test/java/utils.ts
@@ -10,11 +10,30 @@ export async function getEmitOutput(tspCode: string, cb: (program: Program) => C
   const res = render(
     Output().children(
       PackageDirectory({ package: "me.test.code" }).children(
-        SourceFile({ path: "Test.java" }).children(cb(program))
-      )
-    )
+        SourceFile({ path: "Test.java" }).children(cb(program)),
+      ),
+    ),
   );
   const testFile = findFile(res, "Test.java");
+
+  return testFile.contents;
+}
+
+/**
+ * Children will be passed to the package directory, have to declare source file yourself.
+ * Useful util if needing to emit multiple files for the test
+ */
+export async function getMultiEmitOutput(
+  tspCode: string,
+  cb: (program: Program) => Children,
+  testFileName: string = "Test.java",
+) {
+  const program = await getProgram(tspCode);
+
+  const res = render(
+    Output().children(PackageDirectory({ package: "me.test.code" }).children(cb(program))),
+  );
+  const testFile = findFile(res, testFileName);
 
   return testFile.contents;
 }
@@ -33,7 +52,6 @@ export function findFile(res: OutputDirectory, path: string): OutputFile {
         if (item.path.includes(path)) {
           return item;
         }
-        continue;
       } else {
         const found = findFileWorker(item, path);
         if (found) {
