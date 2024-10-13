3555f243e64000cb72d21a00cc14e8931e28de14
Jamin <jaminstratford@gmail.com>
2024-10-13 18:37:14 +1300

    enum cleanup

--- packages/emitter-framework/src/java/components/enum.tsx
+++ packages/emitter-framework/src/java/components/enum.tsx
@@ -1,20 +1,30 @@
 import { mapJoin } from "@alloy-js/core";
 import * as jv from "@alloy-js/java";
+import { useJavaNamePolicy } from "@alloy-js/java";
 import { Enum } from "@typespec/compiler";
 
 export interface EnumProps {
   type: Enum;
 }
 
+/**
+ * Define a Java enum based on the TypeSpec Enum.
+ */
 export function EnumDeclaration({ type }: EnumProps) {
   const members = Array.from(type.members.values());
+  const namePolicy = useJavaNamePolicy();
 
   let valueType: string | undefined;
   let isConsistent = true;
 
   members.forEach((member, index) => {
     if (member.value !== undefined && member.value !== null) {
-      const currentType = typeof member.value === "string" ? "String" : "int";
+      const currentType =
+        typeof member.value === "string"
+          ? "String"
+          : member.value - Math.floor(member.value) !== 0
+            ? "double"
+            : "int";
 
       if (valueType === undefined) {
         valueType = currentType;
@@ -26,7 +36,43 @@ export function EnumDeclaration({ type }: EnumProps) {
 
   // TypeSpec allows you to mix and match values, for a Java enum have to have one value
   if (!isConsistent) {
-    throw new Error("All enum values must have the same type");
+    throw new Error(
+      `Enum '${type.name}' has inconsistent value types. Emitter only supports having one type across all values.`,
+    );
+    // Instead create a class with static fields as we
+    // can mix-and-match types. Accessing is exact same as enums, although just
+    // not an actual Java enum
+    // TODO: Following code works except Alloy needs to allow you to override namepolicy for variable,
+    // TODO: Since right now it's declaring name as lowercase which makes this not work as it should
+    // return (
+    //   <jv.Class public final name={type.name}>
+    //     {mapJoin(
+    //       members,
+    //       (member) => {
+    //         if (!member.value) return null;
+    //         const valueName = namePolicy.getName(member.name, "constant");
+    //         const currentType =
+    //           typeof member.value === "string"
+    //             ? "String"
+    //             : member.value - Math.floor(member.value) !== 0
+    //               ? "double"
+    //               : "int";
+    //         return (
+    //           <jv.Variable
+    //             type={currentType}
+    //             name={valueName}
+    //             value={<jv.Value value={member.value} />}
+    //             public
+    //             static
+    //           />
+    //         );
+    //       },
+    //       { joiner: "\n" },
+    //     )}
+    //
+    //     <jv.Constructor name={type.name} private />
+    //   </jv.Class>
+    // );
   }
 
   let constructor = null;
@@ -41,7 +87,7 @@ export function EnumDeclaration({ type }: EnumProps) {
     );
 
     constructor = (
-      <jv.Constructor name={type.name} parameters={{ value: valueType }} private>
+      <jv.Constructor name={type.name} parameters={{ value: valueType }}>
         this.value = value;
       </jv.Constructor>
     );
